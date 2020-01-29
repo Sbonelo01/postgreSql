2c97b182ac5a1bc2e2dd1d82b8f596f764ad862e
Sbonelo <37469171+Sbonelo01@users.noreply.github.com>
2020-01-29 12:05:42 +0200

    Update umuzi_db_part1.sql
    Inserted data into tables

--- src/umuzi_db_part1.sql
+++ src/umuzi_db_part1.sql
@@ -40,18 +40,48 @@ Description     VARCHAR(300),
 Price           DECIMAL,
 );
 
-
 --order information 
 CREATE TABLE Orders(
 ORDER_ID 			SERIAL primary key,
-Product_ID          INTEGER     REFERENCES Products(Product_ID)
-Payment_ID          INTEGER     REFERENCES Payments(Payment_ID)
-Employee_ID         INTEGER     REFERENCES emplyee(Employee_ID)
+Product_ID          INTEGER     REFERENCES Products(Product_ID),
+Payment_ID          INTEGER     REFERENCES Payments(Payment_ID),
+Employee_ID         INTEGER     REFERENCES Emplyee(Employee_ID),
+Fulfilled_By_Employee_ID INTEGER   REFERENCES Employees(Employee_ID),
 Date_Required       TIMESTAMP,
 Date_Shipped        TIMESTAMP,
 Status				VARCHAR(20)
 );
 
---inserting records to tables
-INSERT INTO Customers(Customer_ID, First_Name, Last_Name, Gender, Address, Phone, Email, City,Country)
- VALUES(1, 'SBONELO', 'MKHIZE', 'MALE', '35 Jules' 0610692364, 'sbonelo@gmail.com', 'Jeppestown', 'South Africa')
+									--inserting records to tables
+
+-- inserting into Customers table
+INSERT INTO Customers (CUSTOMER_ID, First_Name, Last_Name, Gender, Address, Phone, Email, City, Country) VALUES
+(1,	'John', 'Hilbert', 'Male', '284 Chaucer St', 84789657, 'John@gmail.com', 'Johannesburg', 'South Africa'),
+(2,	'Thando', 'Sithole', 'Female', '240 Sect 1', 794445584, 'thando@gmail.com', 'Cape Town', 'South AFrica'),
+(3,	'Leon',	'Glen', 'Male',	'81 Everton Rd, Gillits', 820832830, 'leon@gmail.com', 'Durban', 'South AFrica'),
+(4,	'Charl', 'Muller', 'Male', '290A Dorset Ecke', 856872553, 'charl.muller@yahoo.com', 'Berlin', 'Germany'),
+(5,	'Julia', 'Stein', 'Female', '2 Wernerring',	867244505, 'js234@yahoo.com', 'Frankfurt', 'Germany');
+
+-- inserting into Employees table
+INSERT INTO Employees (EMPLOYEE_ID, First_Name, Last_Name, Email, Job_Title) VALUES
+(1,	'Kani', 'Matthew', 'mat@gmail.com', 'Manager'),
+(2,	'Lesly', 'Cronje', 'lesc@gmail.com', 'Clerk'),
+(3,	'Gideon', 'Maduku', 'm@gmail.com', 'Accountant');
+
+-- inserting into Products table
+INSERT INTO Products (PRODUCT_ID, Product_Name, Description, Price) VALUES
+(1,	'Harley Davidson Chopper', 'This replica features working kickstand, front suspension, gear-shift lever',	150.75),
+(2,	'Classic Car', 'Turnable front wheels, steering function', 550.75),
+(3,	'Sports Car', 'Turnable front wheels, steering function', 700.60);
+
+-- inserting into Payments table
+INSERT INTO Payments (CUSTOMER_ID, Payment_ID, Payment_Date, Amount) VALUES
+(1,	1, '2018-01-09', 150.75),
+(5,	2, '2018-03-09', 150.75),
+(4,	3, '2018-03-09', 700.60);
+
+-- inserting into Orders table
+INSERT INTO Orders (ORDER_ID, Product_ID, Payment_ID, Fulfilled_By_Employee_ID, Date_Required, Date_Shipped, Status) VALUES
+(1,	1, 1, 2, '2018-05-09', NULL, 'Not Shipped'),
+(2,	1, 2, 2, '2018-04-09', '2018-03-09', 'Shipped'),
+(3,	3, 3, 3, '2018-04-09', NULL, 'Not Shipped');
