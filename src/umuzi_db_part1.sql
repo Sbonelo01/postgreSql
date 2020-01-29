2f02d7dc196302d98a679057c28c4509907d74ba
Sbonelo01 <sbonelomkhize15@gmail.com>
2020-01-29 11:04:52 +0200

    fixed my tables

--- src/umuzi_db_part1.sql
+++ src/umuzi_db_part1.sql
@@ -3,7 +3,7 @@ CREATE DATABASE Umuzi;
 
 --create a tables
 CREATE TABLE Customers(
-CUSTOMER_ID 	INTEGER primary key,
+CUSTOMER_ID 	SERIAL primary key,
 First_Name		VARCHAR(50),
 Last_Name		VARCHAR(50),
 Gender			VARCHAR,
@@ -16,30 +16,30 @@ Country			VARCHAR(50)
 
 --emplyee information
 CREATE TABLE Employees(
-EMPLOYEE_ID 	INTEGER primary key,
+EMPLOYEE_ID 	SERIAL primary key,
 First_Name      VARCHAR(50),
 Last_Name       VARCHAR(50),
 Email           VARCHAR(100),
-Job_Title       VARCHAR(20),
+Job_Title       VARCHAR(20)
 );
 
 
 --order information 
 CREATE TABLE Orders(
-ORDER_ID 			INTEGER primary key,
-Product_ID          INTEGER     Foreign Key,
-Payment_ID          INTEGER     Foreign Key,
-Employee_ID         INTEGER     Foreign Key,
-Date_Required       DATE,
-Date_Shipped        DATE,
-Email           	VARCHAR(20),
+ORDER_ID 			SERIAL primary key,
+Product_ID          INTEGER     REFERENCES Products(Product_ID)
+Payment_ID          INTEGER     REFERENCES Payments(Payment_ID)
+Employee_ID         INTEGER     REFERENCES emplyee(Employee_ID)
+Date_Required       TIMESTAMP,
+Date_Shipped        TIMESTAMP,
+Status				VARCHAR(20)
 );
 
 --payment information
 CREATE TABLE Payments(
-PAYMENTS_ID 	INTEGER Foreign key,
-Payment_ID      INTEGER     Primary Key,
-Payment_Date   	DATE,
+PAYMENTS_ID 	SERIAL Primary key,
+Payment_ID      INTEGER		REFERENCES Customers(Customer_ID),
+Payment_Date   	VARCHAR(50),
 Amount         	DECIMAL,
 );
 
