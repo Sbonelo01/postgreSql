810835eab130926b66aa417726bbaf4d5eb226a8
Sbonelo01 <sbonelomkhize15@gmail.com>
2021-01-22 01:03:45 +0200

    naming convention fix

--- src/umuzi_db_part1.sql
+++ src/umuzi_db_part1.sql
@@ -1,21 +1,21 @@
 --create a database called umuzi
-CREATE DATABASE IF NOT EXISTS Umuzi;
+CREATE DATABASE Umuzi;
 
 --create a tables
 CREATE TABLE Customers(
-CUSTOMER_ID 	SERIAL  PRIMARY KEY,
-First_Name		VARCHAR(50),
-Last_Name		VARCHAR(50),
+customer_id 	SERIAL  PRIMARY KEY,
+first_name		VARCHAR(50),
+last_name		VARCHAR(50),
 Gender			VARCHAR,
 Address 		VARCHAR(200),
 Phone			INTEGER,
-Email			VARCHAR(100),
+email			VARCHAR(100),
 City			VARCHAR(20),
 Country			VARCHAR(50)
 
 );
 
-INSERT INTO Customers(First_Name,Last_Name,Gender,Address,Phone,Email,City,Country)
+INSERT INTO Customers(first_name,last_name,Gender,Address,Phone,email,City,Country)
 VALUES('John','Hibert','Male','284 chaucer st','084789657','john@gmail.com','Johannesburg','South Africa'),
 ('Thando','Sithole','Female','240 Sect 1','0794445584','thando@gmail.com','Cape Town','South Africa'),
 ('Leon','Glen','Male','81 Everton Rd,Gillits','0820832830','Leon@gmaiLeonl.com','Durban','South Africa'),
@@ -24,15 +24,15 @@ VALUES('John','Hibert','Male','284 chaucer st','084789657','john@gmail.com','Joh
 
 --emplyee information
 CREATE TABLE Employees(
-EMPLOYEE_ID SERIAL  PRIMARY KEY,
-First_Name      VARCHAR(50),
-Last_Name       VARCHAR(50),
-Email           VARCHAR(100),
-Job_Title       VARCHAR(20)
+employee_id SERIAL  PRIMARY KEY,
+first_name      VARCHAR(50),
+last_name       VARCHAR(50),
+email           VARCHAR(100),
+job_title       VARCHAR(20)
 );
 
 INSERT INTO Employees(
-First_Name,Last_Name,Email,Job_Title
+first_name,last_name,email,job_title
 )
 VALUES('Kani','Matthew','mat@gmail.com','Manager'),
 ('Lesly','Cronje','LesC@gmail.com','Clerk'),
@@ -41,15 +41,15 @@ VALUES('Kani','Matthew','mat@gmail.com','Manager'),
 
 --payment information
 CREATE TABLE Payments(
-CUSTOMER_ID 	INTEGER,
-Payment_ID      SERIAL PRIMARY KEY,
-Payment_Date   	DATE,
-Amount         	DECIMAL,
-FOREIGN KEY (CUSTOMER_ID) REFERENCES Customers (CUSTOMER_ID)
+customer_id 	INTEGER,
+payment_id      SERIAL PRIMARY KEY,
+payment_date   	DATE,
+amount         	DECIMAL,
+FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
 );
 
 INSERT INTO Payments(
-CUSTOMER_ID,Payment_ID,Payment_Date,Amount
+customer_id,payment_id,payment_date,amount
 )
 VALUES('1',DEFAULT,'01-09-2018','150.75'),
 ('5',DEFAULT,'03-09-2018','150.75'),
@@ -57,7 +57,7 @@ VALUES('1',DEFAULT,'01-09-2018','150.75'),
 
 --product information
 CREATE TABLE Products(
-Product_ID      SERIAL PRIMARY KEY,
+product_id      SERIAL PRIMARY KEY,
 Product_Name    VARCHAR(100),
 Description     VARCHAR(300),
 Price           DECIMAL
@@ -73,22 +73,22 @@ VALUES('Harley Davidson Chopper','This replica features working kickstand, front
 
 --order information
 CREATE TABLE Orders(
-ORDER_ID            SERIAL PRIMARY KEY,
-Product_ID          INTEGER,
-Payment_ID          INTEGER,
-Employee_ID         INTEGER,
-Date_Required       DATE,
-Date_Shipped        DATE,
+order_id            SERIAL PRIMARY KEY,
+product_id          INTEGER,
+payment_id          INTEGER,
+employee_id         INTEGER,
+date_required       DATE,
+date_shipped        DATE,
 Status              VARCHAR(20),
-FOREIGN KEY (Product_ID) REFERENCES Products (Product_ID),
-FOREIGN KEY (Payment_ID) REFERENCES Payments (Payment_ID),
-FOREIGN KEY (Employee_ID) REFERENCES Employees (Employee_ID)
+FOREIGN KEY (product_id) REFERENCES Products (product_id),
+FOREIGN KEY (payment_id) REFERENCES Payments (payment_id),
+FOREIGN KEY (employee_id) REFERENCES Employees (employee_id)
 );
 
 
 
 INSERT INTO Orders(
-Product_ID,Payment_ID,Employee_ID,Date_Required,Date_Shipped,Status
+product_id,payment_id,employee_id,date_required,date_shipped,Status
 )
 
 VALUES('1','1','2','05-09-2018',DEFAULT,'Not shipped'),
--- src/umuzi_db_part2.sql
+++ src/umuzi_db_part2.sql
@@ -2,57 +2,57 @@
 SELECT * FROM Customers;
 
 --2.Select records only from the name column in the Customers table.
-SELECT First_Name FROM Customers;
+SELECT first_name FROM Customers;
 
 --3.Show the name of the Customer whose CustomerID is 1.
-SELECT First_Name FROM Customers WHERE Customer_ID=1;
+SELECT first_name FROM Customers WHERE customer_id=1;
 
 --4.Update the record for CustomerID = 1 on the Customer table so that the name is “Lerato Mabitso”.
-UPDATE Customers SET First_Name='Lerato Mabitso' WHERE Customer_ID=1;
+UPDATE Customers SET first_name='Lerato Mabitso' WHERE customer_id=1;
 
 --5.Delete the record from the Customers table for customer 2 (CustomerID = 2).
-DELETE FROM Customers WHERE Customer_ID=2;
+DELETE FROM Customers WHERE customer_id=2;
 
 --6.Select all unique statuses from the Orders table and get a count of the number of orders for each unique status.
 SELECT COUNT (DISTINCT Status) FROM Orders;
 
 --7.Return the maximum payment made on the payments table.
-SELECT MAX(Amount) FROM Payments;
+SELECT MAX(amount) FROM Payments;
 
 --8.Select all customers from the “Customers” table, sorted by the “Country” column.
 SELECT * FROM Customers ORDER BY Country ASC;
 
 --9.SELECT all products with a price BETWEEN R100 and R600.
-SELECT * from products where price between '100' and '600';
+SELECT * from Products where Price between '100' and '600';
 
 --10.SELECT all fields from “Customers” where country is “Germany” AND city is “Berlin”.
-SELECT * from customers
+SELECT * from Customers
 WHERE city='Berlin'
 AND country='Germany';
 
 --11.SELECT all fields from “Customers” where city is “Cape Town” OR “Durban”.
-SELECT * from customers
+SELECT * from Customers
 WHERE city='Cape Town'
 or city='Durban';
 
 --12.SELECT all records from Products where the Price is GREATER than R500.
-SELECT * from products
-WHERE price > '500';
+SELECT * from Products
+WHERE Price > '500';
 
 --13.Return the sum of the Amounts on the Payments table.
-SELECT SUM(amount) FROM payments;
+SELECT SUM(amount) FROM Payments;
 
 --14.Count the number of shipped orders in the Orders table.
-SELECT count(*) from orders
-where status='Shipped';
+SELECT count(*) from Orders
+where Status='Shipped';
 
 --15.Return the average price of all Products, in Rands and in Dollars (assume the exchange rate is R12 to the Dollar).
-SELECT AVG(price) from products;
+SELECT AVG(Price) from Products;
 
 --16.Using INNER JOIN create a query that selects all Payments with Customer information.
 SELECT
-   customers.customer_ID,
-   customers.First_Name,
+   customers.customer_id,
+   customers.first_name,
    customers.Last_Name,
    customers.Email,
    payments.Amount,
@@ -62,9 +62,9 @@ SELECT
    customers.City,
    customers.Address
 FROM
-   customers
-INNER JOIN payments ON customers.customer_ID  =  payments.customer_ID;
+   Customers
+INNER JOIN Payments ON customers.customer_id  =  payments.customer_id;
 
 --17.Select all products that have turnable front wheels.
-SELECT * FROM products 
+SELECT * FROM Products 
 WHERE description LIKE 'Turnable front wheels%';
