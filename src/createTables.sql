be9705a660e62c099e64c86a40d95db280d38245
Sbonelo <sbonelomkhize15@gmail.com>
2021-04-22 15:36:31 +0200

    saparated code and made proper namings

---
+++ src/createTables.sql
@@ -0,0 +1,53 @@
+-- customer table
+CREATE TABLE Customers(
+customer_id 	SERIAL  PRIMARY KEY,
+first_name		VARCHAR(50),
+last_name		VARCHAR(50),
+Gender			VARCHAR,
+Address 		VARCHAR(200),
+Phone			VARCHAR(22),
+email			VARCHAR(100),
+City			VARCHAR(20),
+Country			VARCHAR(50)
+
+);
+
+--emplyees table
+CREATE TABLE Employees(
+employee_id SERIAL  PRIMARY KEY,
+first_name      VARCHAR(50),
+last_name       VARCHAR(50),
+email           VARCHAR(100),
+job_title       VARCHAR(20)
+);
+
+--payments table
+CREATE TABLE Payments(
+customer_id 	INTEGER,
+payment_id      SERIAL PRIMARY KEY,
+payment_date   	DATE,
+amount         	DECIMAL,
+FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+);
+
+--products table
+CREATE TABLE Products(
+product_id      SERIAL PRIMARY KEY,
+Product_Name    VARCHAR(100),
+Description     VARCHAR(300),
+Price           DECIMAL
+);
+
+--orders table
+CREATE TABLE Orders(
+order_id            SERIAL PRIMARY KEY,
+product_id          INTEGER,
+payment_id          INTEGER,
+employee_id         INTEGER,
+date_required       DATE,
+date_shipped        DATE,
+Status              VARCHAR(20),
+FOREIGN KEY (product_id) REFERENCES Products (product_id),
+FOREIGN KEY (payment_id) REFERENCES Payments (payment_id),
+FOREIGN KEY (employee_id) REFERENCES Employees (employee_id)
+);
---
+++ src/pupulateTables.sql
@@ -0,0 +1,40 @@
+
+-- populate customers table
+INSERT INTO Customers(first_name,last_name,Gender,Address,Phone,email,City,Country)
+VALUES('John','Hibert','Male','284 chaucer st','084789657','john@gmail.com','Johannesburg','South Africa'),
+('Thando','Sithole','Female','240 Sect 1','0794445584','thando@gmail.com','Cape Town','South Africa'),
+('Leon','Glen','Male','81 Everton Rd,Gillits','0820832830','Leon@gmaiLeonl.com','Durban','South Africa'),
+('Charl','Muller','Male','290A Dorset Ecke','0856872553','Charl.muller@yahoo.com','Berlin','Germany'),
+('Julia','Julia','Female','2 Wernerring','0672445058','Js234@yahoo.com','Frankfurt','Germany');
+
+-- populate employees table
+INSERT INTO Employees(
+first_name,last_name,email,job_title
+)
+VALUES('Kani','Matthew','mat@gmail.com','Manager'),
+('Lesly','Cronje','LesC@gmail.com','Clerk'),
+('Gideon','Maduku','m@gmail.com','Accountant');
+
+-- populate payments table
+INSERT INTO Payments(
+customer_id,payment_id,payment_date,amount
+)
+VALUES('1',DEFAULT,'01-09-2018','150.75'),
+('5',DEFAULT,'03-09-2018','150.75'),
+('4',DEFAULT,'03-09-2018','700.60');
+
+-- pupulate products table
+INSERT INTO Products(
+Product_Name,Description,Price
+)
+VALUES('Harley Davidson Chopper','This replica features working kickstand, front suspension, gear-shift lever','150.75'),
+('Classic Car','Turnable front wheels, steering function','550.75'),
+('Sports car','Turnable front wheels, steering function','700.60');
+
+-- populate orders table
+INSERT INTO Orders(
+product_id,payment_id,employee_id,date_required,date_shipped,Status
+)
+VALUES('1','1','2','05-09-2018',DEFAULT,'Not shipped'),
+('1','2','2','04-09-2018','03-09-2018','Shipped'),
+('3','3','3','06-09-2018',DEFAULT,'Not shipped');
